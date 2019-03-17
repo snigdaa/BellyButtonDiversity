7ea6b08fc59c3cd8e0c0fb9cd0be8fbd903d8406
Sairathi <snigdaa.ram@gmail.com>
2019-03-17 00:17:45 -0400

    testing heroku and install versions of flask

--- bellybutton/static/js/app.js
+++ bellybutton/static/js/app.js
@@ -13,10 +13,73 @@ function buildMetadata(sample) {
       row.text(`${key} : ${value}`);
       console.log(row.textContent)
     })
+
+    //Build a Gauge chart that gauges the wash frequency of the subject
+    var wfreq = totDict.WFREQ;
+    //since 180/6 = 20, we're binding the angle to the wfreq value according to the sections of the gauge
+    var angle = 180 - (wfreq * 20),
+        // length of pointer ratio to gauge
+        radius = 0.5;
+    var radians = angle * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    var mainPath = 'M -.0 -0.05 L .0 0.05 L ',
+        xPath = String(x),
+        space = ' ',
+        yPath = String(y),
+        endPath = ' Z';
+    var path = mainPath.concat(xPath, space, yPath, endPath);
+
+    //define the pie chart that we'll make into a gauge
+    var data = [{ type: 'scatter',
+      x: [0], y:[0],
+        marker: {size: 1, color:'850000'},
+        showlegend: false,
+        name: 'Washes',
+        text: wfreq,
+        hoverinfo: 'text+name'},
+      { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+      rotation: 90,
+      text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3',
+                '1-2', '0-1', ''],
+      textinfo: 'text',
+      textposition:'inside',
+      marker: {colors:['rgba(0, 50, 0, .5)', 'rgba(0, 75, 0, .5)',
+                            'rgba(0, 100, 0, .5)', 'rgba(14, 127, 0, .5)',
+                            'rgba(110, 154, 22, .5)', 'rgba(170, 202, 42, .5)',
+                            'rgba(202, 209, 95, .5)', 'rgba(210, 206, 145, .5)',
+                            'rgba(232, 226, 202, .5)', 'rgba(255, 255, 255, 0)']},
+      labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
+
+    //define layout for gauge
+    var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          // Set the color for the pointer
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      title: '<b> Belly Button Wash Frequency </b><br> Scrubs per Week',
+      xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+    };
+
+    //plot gauge based on wash frequency
+    var GAUGE = document.getElementById("gauge");
+    Plotly.newPlot(GAUGE, data, layout);
   })
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
 }
 
 function buildCharts(sample) {
